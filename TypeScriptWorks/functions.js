8ce284f8dd3f7352312d7644d22cc25ac78ac4ac
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-07 11:08:53 +0300

    functions

---
+++ TypeScriptWorks/functions.js
@@ -0,0 +1,13 @@
+function sum(x, y) {
+    return x + y;
+}
+function sum2(x, y) {
+    return x + y;
+}
+var sum3 = function (x, y) {
+    return x + y;
+};
+console.log(sum(2, 3));
+console.log(sum("İstnabul", 2));
+console.log(sum2(2, 4));
+console.log(sum3(4, 8));
---
+++ TypeScriptWorks/functions.ts
@@ -0,0 +1,16 @@
+function sum(x,y){
+    return x+y;
+}
+
+function sum2(x:number,y:number):number{
+    return x+y;
+}
+
+let sum3 = function(x:number,y:number):number{
+    return x+y;
+}
+
+console.log(sum(2,3));
+console.log(sum("İstnabul",2));
+console.log(sum2(2,4));
+console.log(sum3(4,8));
