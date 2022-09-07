9391b96a31d44be941076864d8f100c117367fb7
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-07 10:38:42 +0300

    intro typescript

---
+++ TypeScriptWorks/intro.js
@@ -0,0 +1,5 @@
+function greeter(name) {
+    return "Hello " + name;
+}
+var message = greeter("Ehlullah 2");
+console.log(message);
---
+++ TypeScriptWorks/intro.ts
@@ -0,0 +1,7 @@
+function greeter(name:string){
+    return "Hello " + name
+}
+
+let message = greeter("Ehlullah 2")
+
+console.log(message)
