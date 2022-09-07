d194584969ae8d0d756c706be283e465d22730db
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-07 11:17:14 +0300

    optional parameters

--- TypeScriptWorks/functions.js
+++ TypeScriptWorks/functions.js
@@ -11,3 +11,15 @@ console.log(sum(2, 3));
 console.log(sum("İstnabul", 2));
 console.log(sum2(2, 4));
 console.log(sum3(4, 8));
+function sum4(x, y) {
+    if (y === void 0) { y = 4; }
+    return x + y;
+}
+console.log(sum4(3));
+function sum5(x, y) {
+    if (y) {
+        return x + y;
+    }
+    return x;
+}
+console.log(sum5(3, 40));
--- TypeScriptWorks/functions.ts
+++ TypeScriptWorks/functions.ts
@@ -14,3 +14,19 @@ console.log(sum(2,3));
 console.log(sum("İstnabul",2));
 console.log(sum2(2,4));
 console.log(sum3(4,8));
+
+function sum4(x:number,y:number = 4):number{
+    return x+y;
+}
+
+console.log(sum4(3));
+
+
+function sum5(x:number,y?:number):number{
+    if(y) {
+        return x+y;
+    }
+    return x;
+}
+
+console.log(sum5(3,40));
