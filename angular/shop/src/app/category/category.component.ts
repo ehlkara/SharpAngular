340b8b26a776830b5180d0743c651a2c100b0a2a
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-08 10:47:37 +0300

    added category design

--- angular/shop/src/app/category/category.component.ts
+++ angular/shop/src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
+
+  title = "Category List"
+  categories : Category[] = [
+    {id:1, name:"Electronic"},
+    {id:2, name:"Computers"},
+    {id:3, name:"Phones"},
+    {id:4, name:"Music"},
+    {id:5, name:"Drinks"}
+  ]
 
   ngOnInit(): void {
   }
