fa3a42191d61c73bff61cc5a3c9338fb06d14e7d
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-08 10:38:25 +0300

    added product object

--- angular/shop/src/app/product/product.component.ts
+++ angular/shop/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -10,13 +11,13 @@ export class ProductComponent implements OnInit {
   constructor() { }
 
   title = "Product List"
-  products : any[] = [
-    {id:1, name: "NoteBook", price:25000, categoryId:1, description:"HP Pavilion Gaming"},
-    {id:2, name: "Mouse", price:25, categoryId:2, description:"Microsoft mouse"},
-    {id:1, name: "NoteBook", price:25000, categoryId:1, description:"HP Pavilion Gaming"},
-    {id:2, name: "Mouse", price:25, categoryId:2, description:"Microsoft mouse"},
-    {id:1, name: "NoteBook", price:25000, categoryId:1, description:"HP Pavilion Gaming"},
-    {id:2, name: "Mouse", price:25, categoryId:2, description:"Microsoft mouse"}
+  products : Product[] = [
+    {id:1, name: "NoteBook", price:25000, categoryId:1, description:"HP Pavilion Gaming",imageUrl:"https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"},
+    {id:2, name: "Mouse", price:25, categoryId:2, description:"Microsoft mouse",imageUrl:"https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"},
+    {id:1, name: "NoteBook", price:25000, categoryId:1, description:"HP Pavilion Gaming",imageUrl:"https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"},
+    {id:2, name: "Mouse", price:25, categoryId:2, description:"Microsoft mouse",imageUrl:"https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"},
+    {id:1, name: "NoteBook", price:25000, categoryId:1, description:"HP Pavilion Gaming",imageUrl:"https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"},
+    {id:2, name: "Mouse", price:25, categoryId:2, description:"Microsoft mouse",imageUrl:"https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"}
   ] 
 
   ngOnInit(): void {
