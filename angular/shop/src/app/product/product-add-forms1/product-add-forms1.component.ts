b5ffac8fe9a0ab433904c43c2775922034c022ce
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-08 16:41:22 +0300

    created clasic form

--- angular/shop/src/app/product/product-add-forms1/product-add-forms1.component.ts
+++ angular/shop/src/app/product/product-add-forms1/product-add-forms1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Category } from 'src/app/category/category';
 import { CategoryService } from 'src/app/services/category.service';
 import { Product } from '../product';
@@ -7,18 +8,22 @@ import { Product } from '../product';
   selector: 'app-product-add-forms1',
   templateUrl: './product-add-forms1.component.html',
   styleUrls: ['./product-add-forms1.component.css'],
-  providers:[CategoryService]
+  providers: [CategoryService]
 })
 export class ProductAddForms1Component implements OnInit {
 
-  constructor(private categoryService:CategoryService) { }
+  constructor(private categoryService: CategoryService) { }
 
-  model : Product = new Product();
+  model: Product = new Product();
   categories!: Category[];
 
   ngOnInit(): void {
     this.categoryService.getcategories().subscribe(data => {
       this.categories = data
     });
+  }
+
+  add(form: NgForm) {
+    console.log(form.value.name);
   }
 }
