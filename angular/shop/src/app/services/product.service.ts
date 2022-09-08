86ca59ef1d4b51454cc85cbdefa9211b2d0166e2
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-08 16:54:27 +0300

    add product with classic html form

--- angular/shop/src/app/services/product.service.ts
+++ angular/shop/src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Product } from '../product/product';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators'
@@ -12,13 +12,25 @@ export class ProductService {
   path = "http://localhost:3000/products"
 
   getProducts(categoryId: number): Observable<Product[]> {
-
     let newPath = this.path;
     if (categoryId) {
       newPath += "?categoryId=" + categoryId
     }
 
     return this.http.get<Product[]>(newPath).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  addProduct(product: Product): Observable<Product> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      })
+    }
+    return this.http.post<Product>(this.path, product,httpOptions).pipe(
       tap(data => console.log(JSON.stringify(data))),
       catchError(this.handleError)
     );
