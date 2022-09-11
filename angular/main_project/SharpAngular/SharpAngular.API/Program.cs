8182246a04642951515dcf7a41285d2792950eb7
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-11 12:26:36 +0300

    created n layer archtecture

--- angular/main_project/SharpAngular/SharpAngular.API/Program.cs
+++ angular/main_project/SharpAngular/SharpAngular.API/Program.cs
@@ -1,6 +1,12 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+//});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
