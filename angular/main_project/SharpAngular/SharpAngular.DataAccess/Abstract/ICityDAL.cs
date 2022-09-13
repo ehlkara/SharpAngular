33e0b80c0360ed0c2319dc2f2fca44da619a7647
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-13 10:18:28 +0300

    updated package

--- angular/main_project/SharpAngular/SharpAngular.DataAccess/Abstract/ICityDAL.cs
+++ angular/main_project/SharpAngular/SharpAngular.DataAccess/Abstract/ICityDAL.cs
@@ -1,9 +1,4 @@
 ﻿using SharpAngular.Models.Entities.SharpAngular;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharpAngular.DataAccess.Abstract
 {
