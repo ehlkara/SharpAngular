dc3f9037015924da633340fd46744195f74d7ed1
ehlkara <ehlullah.fb@gmail.com>
2022-09-19 10:24:20 +0300

    finished authService

--- angular/main_project/SharpAngular.Web/src/app/services/auth.service.ts
+++ angular/main_project/SharpAngular.Web/src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginUser } from '../models/loginUser';
-import { JwtHelper } from 'angular2-jwt';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 import { Router } from '@angular/router';
 import { AlertifyService } from './alertify.service';
+import { RegisterUser } from '../models/registerUser';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,7 @@ export class AuthService {
   userToken: any;
   decodedToken: any;
   jwtHelper: JwtHelper = new JwtHelper();
+  TOKEN_KEY = "accessToken";
 
   login(loginUser: LoginUser) {
     let headers = new HttpHeaders();
@@ -33,7 +35,32 @@ export class AuthService {
     });
   }
 
-  saveToken(token) {
-    localStorage.setItem('token', token);
+  register(registerUser: RegisterUser) {
+    let headers = new HttpHeaders();
+    headers = headers.append("Content-Type", "application/json");
+    this.httpClient.post(this.path + "register", registerUser, { headers: headers }).subscribe(data => {
+
+    })
   }
+
+  saveToken(token) {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  logOut() {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  loggedIn() {
+    return tokenNotExpired(this.TOKEN_KEY);
+  }
+
+  get token() {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  getCurrentUserId() {
+    return this.jwtHelper.decodeToken(this.token).id;
+  }
+
 }
